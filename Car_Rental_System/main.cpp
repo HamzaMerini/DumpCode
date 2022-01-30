4bb68ad5da611f1bc5ed3b6a7da90d187a3f5ba8
hmerini <merinimhamza@gmail.com>
2022-01-30 14:45:44 +0100

    Car Project from scratch

--- Car_Rental_System/main.cpp
+++ Car_Rental_System/main.cpp
@@ -1,8 +1,54 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<conio.h>
 
 using namespace std;
+
+class user{
+    private:
+    string password="pass";
+
+    public:
+    void Login(){
+        char ch;
+        string pass;
+        cout<<"\n\t\t\t\t\tEnter Password : ";
+        ch = _getch();
+        while(ch != 13){//character 13 is enter
+        pass.push_back(ch);
+        cout << '*';
+        ch = _getch();
+        }
+        cout << "\n";
+    
+        if (pass==password){
+            cout << "\t\t\t\t\tSuccesfly logged in!"<<endl;
+            return;
+        }
+        else
+            cout << "\t\t\t\t\tWrong Password \n\t\t\t\t\t\tTry again !"<<endl;
+            cout << "	----------------------------------------------------------------------------------------------"<<endl;
+  
+            Login();
+    }
+
+};
+
+// Name - Driving licence N. - historique
+class customer{
+    public:
+    string name,licence;
+
+    
+};
+
+// Model - Plate N. - characteristics - price - rent calcul - available cars
+class car{
+    public:
+    string model;
+
+};
 
 void welcome(){
     ifstream welcome_file("welcome.txt");
@@ -11,13 +57,15 @@ void welcome(){
         cout << welcome_file.rdbuf();
     }
     cout << "\n";
-}
+    cout << "	----------------------------------------------------------------------------------------------"<<endl;
+  }
 
-int main()
-{   
-    string name;
+
+int main(){   
+   //customer A;
+    user A;
     welcome();
-    cin >> name;
-    cout << "\n \tWelcome "<<name<<endl;
+    A.Login();
     system("PAUSE");
+    
 }
